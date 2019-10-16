6acd00a32748da9a11632c287004e821974cf6d6
Steven Vorona <stevenvorona@gmail.com>
2019-10-15 17:04:47 -0700

    Updated RDS endpoint

---
+++ config.py
@@ -0,0 +1,6 @@
+# edit the URI below to add your RDS password and your AWS URL
+# The other elements are the same as used in the tutorial
+# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
